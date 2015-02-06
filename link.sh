a8c507092ff7acd706329d6859dcf162c32003b7
Adam Biggs <adam.biggs@lightmaker.com>
2015-02-06 12:17:18 -0800

    feat: added tmuxinator config to the repo

--- link.sh
+++ link.sh
@@ -7,5 +7,6 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 echo -e "\n${COLOR}Linking dotfiles into your user directory...${RESET}"
 ln -si $SCRIPTPATH/.nvimrc ~/.nvimrc
 ln -si $SCRIPTPATH/.tmux.conf ~/.tmux.conf
+ln -si $SCRIPTPATH/.tmuxinator ~/.tmuxinator
 ln -si $SCRIPTPATH/.zshrc ~/.zshrc
 echo -e "${GREEN}Done.${RESET}\n"

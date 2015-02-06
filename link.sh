fe9d3d2eccce581a96ffbc07a1c0bcdaa290fd8b
Adam Biggs <adam.biggs@lightmaker.com>
2015-02-06 12:16:58 -0800

    fix: always use absolute path of `link.sh` script when creating symlinks

--- link.sh
+++ link.sh
@@ -2,11 +2,10 @@
 COLOR=`tput setaf 4`
 GREEN=`tput setaf 2`
 RESET=`tput sgr0`
-DIR=`dirname "$0"` # Find the relative path of the script.
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-# Link dotfiles from this repo to your user directory
 echo -e "\n${COLOR}Linking dotfiles into your user directory...${RESET}"
-ln -s $DIR/.zshrc ~/.zshrc
-ln -s $DIR/.tmux.conf ~/.tmux.conf
-ln -s $DIR/.nvimrc ~/.nvimrc
+ln -si $SCRIPTPATH/.nvimrc ~/.nvimrc
+ln -si $SCRIPTPATH/.tmux.conf ~/.tmux.conf
+ln -si $SCRIPTPATH/.zshrc ~/.zshrc
 echo -e "${GREEN}Done.${RESET}\n"

e402e81b2d511b1df6dd14839ad091a59e6c15c0
Adam Biggs <adam.biggs@lightmaker.com>
2015-02-16 11:27:57 -0800

    feat: added `.zlogin` file

--- link.sh
+++ link.sh
@@ -10,6 +10,7 @@ ln -shiv $SCRIPTPATH/.nvimrc ~/.nvimrc
 ln -shiv $SCRIPTPATH/.tmux.conf ~/.tmux.conf
 ln -shiv $SCRIPTPATH/.tmuxinator ~/.tmuxinator
 ln -shiv $SCRIPTPATH/.zshrc ~/.zshrc
+ln -shiv $SCRIPTPATH/.zlogin ~/.zlogin
 echo -e "${good}Done.${clear}"
 
 echo -e "\n${info}Linking Vim spellfiles to NeoVim...${clear}"

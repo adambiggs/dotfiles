772dc0310a6c2c7b737128d7c7c4f749716dada1
Adam Biggs <adam.biggs@lightmaker.com>
2015-03-22 19:35:21 -0700

    feat: update script (wip)

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+info=`tty -s && tput setaf 4`
+good=`tty -s && tput setaf 2`
+bad=`tty -s && tput setaf 1`
+clear=`tty -s && tput sgr0`
+
+# Homebrew
+echo -e "\n${info}Updating Homebrew packages...${clear}"
+brew update
+brew upgrade
+#brew reinstall --HEAD neovim # TODO: check for updates before reisntalling...
+echo -e "${good}Done.${clear}"
+
+# pip
+echo -e "\n${info}Updating certain pip packages...${clear}"
+sudo pip install -U neovim awscli
+echo -e "${good}Done.${clear}"
+
+# gem
+echo -e "\n${info}Updating certain ruby gems...${clear}"
+gem update tmuxinator CoffeeTags
+echo -e "${good}Done.${clear}"
+
+# zgen
+# TODO: Not working...
+#echo -e "\n${info}Updating zsh plugins...${clear}"
+#zgen selfupdate
+#zgen update
+#echo -e "${good}Done.${clear}"

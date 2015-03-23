fa3bac6b667f1059ba99b5a9f22f66cb81dda96f
Adam Biggs <adam.biggs@lightmaker.com>
2015-03-22 19:22:19 -0700

    feat: more better install script

--- install.sh
+++ install.sh
@@ -1,13 +1,59 @@
-#!/bin/bash                                                                                                                        │  1
+#!/bin/bash
+info=`tty -s && tput setaf 4`
+good=`tty -s && tput setaf 2`
+bad=`tty -s && tput setaf 1`
+clear=`tty -s && tput sgr0`
+
+# Install a Homebrew package if not already installed.
+function install_brew_pkg() {
+  pkg_name=$1
+  pkg_installed=`brew ls | grep $pkg_name`
+  if [ -z $pkg_installed ]; then
+    if [ $pkg_name == 'neovim' ]; then
+      brew tap neovim/homebrew-neovim
+      brew install --HEAD neovim
+    else
+      brew install $pkg_name
+    fi
+  fi
+}
 
 # Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Tools
-brew install zsh git tmux the_silver_searcher wget
+echo -e "\n${info}Installing Homebrew packages...${clear}"
+if ! type brew > /dev/null; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+install_brew_pkg neovim
+install_brew_pkg zsh
+install_brew_pkg git
+install_brew_pkg tmux
+install_brew_pkg the_silver_searcher
+install_brew_pkg wget
+echo -e "${good}Done.${clear}"
 
 # Use zsh as default shell.
-chsh -s `which zsh`
+echo -e "\n${info}Switching default shell to ZSH...${clear}"
+ZSH_DIR=`whereis zsh`
+DEFAULT_SHELL=`finger $LOGNAME | grep Shell | awk -F':' '{print $3}'`
+if [ $ZSH_DIR == $DEFAULT_SHELL ]; then
+  echo -e "${good}Default shell already using${clear}$DEFAULT_SHELL${good}.${clear}"
+else
+  chsh -s `which zsh`
+  echo -e "${good}Done.${clear}"
+fi
+
+# Install Git submodules.
+echo -e "\n${info}Installing Git submodules...${clear}"
+git submodule init
+git submodule update
+echo -e "${good}Done.${clear}"
 
 # Install vim-plugged.
-curl -fLo ~/.nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo -e "\n${info}Installing Vim-Plug...${clear}"
+VIMPLUG_PATH=~/.nvim/autoload/plug.vim
+if [ -e "$VIMPLUG_PATH" ]; then
+  echo -e "${good}Already installed.${clear}\n"
+else
+  curl -fLo ~/.nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  echo -e "${good}Done.${clear}\n"
+fi

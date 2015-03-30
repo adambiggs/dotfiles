8734d21e33f8b8149e6aa2b22c575ff7c8a013a8
Adam Biggs <adam.biggs@lightmaker.com>
2015-03-30 12:33:17 -0700

    feat(link): make link script non-interactive (regenerate links without asking)

--- link.sh
+++ link.sh
@@ -6,16 +6,16 @@ clear=`tty -s && tput sgr0`
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 echo -e "\n${info}Linking dotfiles into your user directory...${clear}"
-ln -shiv $SCRIPTPATH/.nvimrc ~/.nvimrc
-ln -shiv $SCRIPTPATH/.tmux.conf ~/.tmux.conf
-ln -shiv $SCRIPTPATH/.tmuxinator ~/.tmuxinator
-ln -shiv $SCRIPTPATH/.zshrc ~/.zshrc
-ln -shiv $SCRIPTPATH/.zlogin ~/.zlogin
+ln -sfv $SCRIPTPATH/.nvimrc ~/.nvimrc
+ln -sfv $SCRIPTPATH/.tmux.conf ~/.tmux.conf
+ln -sfv $SCRIPTPATH/.tmuxinator ~/.tmuxinator
+ln -sfv $SCRIPTPATH/.zshrc ~/.zshrc
+ln -sfv $SCRIPTPATH/.zlogin ~/.zlogin
 echo -e "${good}Done.${clear}"
 
 if [[ $OSTYPE == darwin* ]]; then
   echo -e "\n${info}Linking custom Karabiner key mappings...${clear}"
-  ln -shiv $SCRIPTPATH/prefs/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml
+  ln -sfv $SCRIPTPATH/prefs/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml
   echo -e "${good}Done.${clear}"
 fi
 

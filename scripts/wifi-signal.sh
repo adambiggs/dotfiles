0965ed2f66406282c1d40214269b2a70dfcccf03
Adam Biggs <adam.biggs@lightmaker.com>
2015-07-03 09:44:58 -0700

    feat(tmux): added icon to wifi status line module

--- scripts/wifi-signal.sh
+++ scripts/wifi-signal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 REPORT=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I`
-SSID=`echo "$REPORT" | grep " SSID" | sed -e 's/^.* //g'`
-SIGNAL=`echo "$REPORT" | grep CtlRSSI | sed -e 's/^.*://g'`
-printf "[$SSID]$SIGNAL dBm"
+SSID=`echo "$REPORT" | grep " SSID" | sed -e 's/^.*: //g'`
+SIGNAL=`echo "$REPORT" | grep CtlRSSI | sed -e 's/^.*: //g'`
+printf "  $SSID [$SIGNAL dBm]"

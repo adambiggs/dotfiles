4e4dd67b4c572c1d171012c2230aaf30c31176f6
Adam Biggs <adam.biggs@lightmaker.com>
2015-08-25 11:46:06 -0700

    feat(tmux): improved WiFi status line icon 🍕

--- scripts/wifi-signal.sh
+++ scripts/wifi-signal.sh
@@ -2,4 +2,4 @@
 REPORT=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I`
 SSID=`echo "$REPORT" | grep " SSID" | sed -e 's/^.*: //g'`
 SIGNAL=`echo "$REPORT" | grep CtlRSSI | sed -e 's/^.*: //g'`
-printf "  $SSID [$SIGNAL dBm]"
+printf "  $SSID [$SIGNAL dBm]"

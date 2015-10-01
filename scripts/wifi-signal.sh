d6f23ab759031d8b972b6af811d20dfbf09a0938
Adam Biggs <adam.biggs@lightmaker.com>
2015-09-30 23:26:30 -0700

    feat(tmux): fancy new wifi strength meter

--- scripts/wifi-signal.sh
+++ scripts/wifi-signal.sh
@@ -2,4 +2,23 @@
 REPORT=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I`
 SSID=`echo "$REPORT" | grep " SSID" | sed -e 's/^.*: //g'`
 SIGNAL=`echo "$REPORT" | grep CtlRSSI | sed -e 's/^.*: //g'`
-printf "  $SSID [$SIGNAL dBm]"
+
+if [ "$SIGNAL" -gt "-60" ]; then
+  WIFI_BARS="●●●●"
+elif [ "$SIGNAL" -lt "-60" ]; then
+  WIFI_BARS="●●●◯"
+elif [ "$SIGNAL" -lt "-70" ]; then
+  WIFI_BARS="●●◯◯"
+elif [ "$SIGNAL" -lt "-80" ]; then
+  WIFI_BARS="●◯◯◯"
+else
+  WIFI_BARS="◯◯◯◯"
+fi
+
+MSG="$SSID $WIFI_BARS"
+
+if [ -z "$SSID" ];then
+  MSG="No WiFi"
+fi
+
+printf "  $MSG"

35716d1642037ae77c578bbf38ee5a615296d538
Adam Biggs <adam.biggs@lightmaker.com>
2015-09-30 23:00:55 -0700

    feat(tmux): tweaked battery icon percentages

--- scripts/battery-icon.sh
+++ scripts/battery-icon.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 BATTERY_PERCENTAGE=`$SCRIPT_PATH/battery-status/scripts/battery_percentage.sh`
 BATTERY_VALUE="${BATTERY_PERCENTAGE%?}"
 
-if [ "$BATTERY_VALUE" -gt "75" ]; then
+if [ "$BATTERY_VALUE" -gt "90" ]; then
   ICON=" "
 elif [ "$BATTERY_VALUE" -gt "50" ]; then
   ICON=" "
-elif [ "$BATTERY_VALUE" -gt "25" ]; then
+elif [ "$BATTERY_VALUE" -gt "30" ]; then
   ICON=" "
 elif [ "$BATTERY_VALUE" -gt "10" ]; then
   ICON=" "

906da41f3557cb578ca7983845f61ca0524b77d7
Adam Biggs <adam.biggs@lightmaker.com>
2016-03-31 13:55:32 -0700

    feat(find-and-replace): escape find/replace params and tell user what's going on

--- scripts/find-and-replace.sh
+++ scripts/find-and-replace.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-DIR=$1
-PATTERN=$2
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+source $SCRIPT_PATH/colors
 
-LC_ALL=C find $DIR -type f -exec sed -i '' -e $PATTERN {} \;
+# Input.
+FIND=$(echo $1 | sed -e 's/[\/&]/\\&/g')
+REPLACE=$(echo $2 | sed -e 's/[\/&]/\\&/g')
+DIR=$3
+
+# Build & run the command.
+SED_CMD=s/$FIND/$REPLACE/g
+echo -ne "${clear}Running sed replacement ${blue}$SED_CMD${clear} on files matching ${blue}$DIR${clear}... "
+LC_ALL=C find $DIR -type f -exec sed -i '' -e $SED_CMD {} \;
+echo -e "${green}Done${clear}"
